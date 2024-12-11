2aeb030ca62173e7dc1f97c4a9d53b2705e8d3db
kunal <kunal4103@gmail.com>
2024-12-11 19:46:36 +0530

    improved design

--- app/page.tsx
+++ app/page.tsx
@@ -38,17 +38,17 @@ function CustomGridDemo() {
   const testimonials = [
     {
       quote:
-        "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
-      name: "Sarah Chen",
-      designation: "Product Manager at TechFlow",
-      src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "Kunal consistently delivers his freelance projects quickly and efficiently, demonstrating exceptional teamwork and strong work ethics. His dedication and professionalism make him a valuable asset to any team.",
+      name: "Ishaan Gupta",
+      designation: "Satisfied Client",
+      src: "/images/ishaan.jpg",
     },
     {
       quote:
-        "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
-      name: "Michael Rodriguez",
-      designation: "CTO at InnovateSphere",
-      src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "I’ve witnessed his exceptional skill in both full-stack development and machine learning, consistently delivering robust, user-focused solutions. His technical expertise and results-driven approach make him invaluable to any project or team.",
+      name: "Vibhanshu Pandey",
+      designation: "Satisfied Client",
+      src: "/images/vibhanshu.jpg",
     },
     {
       quote:
